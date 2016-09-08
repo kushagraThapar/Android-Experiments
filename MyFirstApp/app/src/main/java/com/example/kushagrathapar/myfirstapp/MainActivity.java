36268c1787476ecad8995e2753e72cc6d6da99df
Kushagra Thapar <kthapa2@uic.edu>
2016-09-08 09:03:44 -0500

    Adding Display Message Activity

--- MyFirstApp/app/src/main/java/com/example/kushagrathapar/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/kushagrathapar/myfirstapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.kushagrathapar.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.kushagrathapar.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
